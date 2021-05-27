f9525534c00c2db93d44f7566dde3b164bfe9d49
root <root@ip-172-31-44-1.ap-south-1.compute.internal>
2021-05-27 19:14:00 +0000

    main.tf updated

--- hello_terraform/main.tf
+++ hello_terraform/main.tf
@@ -1,16 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.40.0"
+    }
+  }
+}
+
 provider "aws" {
-  version    = "3.4.0"
   region     = "us-east-1"
-  access_key = "AKIAJAPEC2Z3LMPOQ2PQ"
-  secret_key = "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"
+  access_key = "AKIAQXKGRSY2GBUJQZWS"
+  secret_key = "GuBGpAV6TfuDp9c0O12ADHB+W4WTMDSlBAVf/KvN"
 }
 
 resource "aws_instance" "example" {
-  ami = "ami-098f16afa9edf40be"
-	instance_type = "t2.micro"
-	key_name      = "aws"
+  ami           = "ami-098f16afa9edf40be"
+  instance_type = "t2.micro"
+  key_name      = "awskey"
 
-	tags = {
-	  Name = "Terraform Created"
-	}
+  tags = {
+    Name = "Terraform Created"
+  }
 }
